7c71b3e40126786998673efd47c0fb2635acb634
milliceo <millimceo@gmail.com>
2025-01-16 01:46:12 +0900

    test10

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-const emailInput = document.getElementById("emailInput");
-const pwInput = document.getElementById("pwInput");
-const warningEmailText = document.getElementById("emailInputWarning");
-const warningPwText = document.getElementById("pwInputWarning");
-
-warningEmailText.style.display = "none";
-warningPwText.style.display = "none";
-
 // 이메일 입력 필드 진입&벗어날 때 경고 메시지 숨기기입니다.
+const emailInput = document.getElementById("emailInput");
+const warningEmailText = document.getElementById("emailInputWarning");
+warningEmailText.style.display = "none";
+
 emailInput.addEventListener("input", function () {
 	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 	if (!emailPattern.test(emailInput.value)) {
@@ -20,7 +16,11 @@ emailInput.addEventListener("blur", function () {
 	warningEmailText.style.display = "none";
 });
 
-// 이메일 입력 필드 진입&벗어날 때 경고 메시지 숨기기입니다.
+// 비밀번호 입력 필드 진입&벗어날 때 경고 메시지 숨기기입니다.
+const pwInput = document.getElementById("pwInput");
+const warningPwText = document.getElementById("pwInputWarning");
+warningPwText.style.display = "none";
+
 pwInput.addEventListener("input", function () {
 	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 	if (!emailPattern.test(emailInput.value)) {
@@ -41,4 +41,12 @@ document.getElementById("openOverlayBtn").addEventListener("click", function () 
 
 document.getElementById("closeOverlayBtn").addEventListener("click", function () {
 	document.getElementById("overlay").style.display = "none";
+});
+
+document.getElementById("sendVerificationBtn").addEventListener("click", function () {
+	document.getElementById("codeSentOverlay").style.display = "flex";
+});
+
+document.getElementById("closeSuccessBtn").addEventListener("click", function () {
+	document.getElementById("codeSentOverlay").style.display = "none";
 });
