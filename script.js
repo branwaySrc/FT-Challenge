fb28e1c1ee06fd122deb75bcfaf9f354ee9c2042
milliceo <millimceo@gmail.com>
2025-01-15 19:23:01 +0900

    test 6

---
+++ script.js
@@ -0,0 +1,35 @@
+const emailInput = document.getElementById("emailInput");
+const pwInput = document.getElementById("pwInput");
+const warningEmailText = document.getElementById("emailInputWarning");
+const warningPwText = document.getElementById("pwInputWarning");
+
+warningEmailText.style.display = "none";
+warningPwText.style.display = "none";
+
+// 이메일 입력 필드 진입&벗어날 때 경고 메시지 숨기기
+emailInput.addEventListener("input", function () {
+	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+	if (!emailPattern.test(emailInput.value)) {
+		warningEmailText.style.display = "block";
+	} else {
+		warningEmailText.style.display = "none";
+	}
+});
+
+emailInput.addEventListener("blur", function () {
+	warningEmailText.style.display = "none";
+});
+
+// 비번 입력 필드 진입&벗어날 때 경고 메시지 숨기기
+pwInput.addEventListener("input", function () {
+	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+	if (!emailPattern.test(emailInput.value)) {
+		warningPwText.style.display = "block";
+	} else {
+		warningPwText.style.display = "none";
+	}
+});
+
+pwInput.addEventListener("blur", function () {
+	warningPwText.style.display = "none";
+});
