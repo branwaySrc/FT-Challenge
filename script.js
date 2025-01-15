be73914993be8bccfb6d44ab6e713337c0a9aee0
milliceo <millimceo@gmail.com>
2025-01-15 20:00:19 +0900

    test 8

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const warningPwText = document.getElementById("pwInputWarning");
 warningEmailText.style.display = "none";
 warningPwText.style.display = "none";
 
-// 이메일 입력 필드 진입&벗어날 때 경고 메시지 숨기기
+// 이메일 입력 필드 진입&벗어날 때 경고 메시지 숨기기입니다.
 emailInput.addEventListener("input", function () {
 	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 	if (!emailPattern.test(emailInput.value)) {
@@ -20,7 +20,7 @@ emailInput.addEventListener("blur", function () {
 	warningEmailText.style.display = "none";
 });
 
-// 비번 입력 필드 진입&벗어날 때 경고 메시지 숨기기
+// 이메일 입력 필드 진입&벗어날 때 경고 메시지 숨기기입니다.
 pwInput.addEventListener("input", function () {
 	const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
 	if (!emailPattern.test(emailInput.value)) {
